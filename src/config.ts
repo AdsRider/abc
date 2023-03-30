4ae2acbd7f0fc7736c812dd1ee4a4385bf8cf01b
Lurang <111482387+Lurang@users.noreply.github.com>
2023-03-30 18:01:29 +0900

    initial database commit (#6)
    * initial database commit
    
    * add slonik lib
    
    * add database config
    
    * create database pool
    
    * add address to db module
    
    * change addAddress return type
    
    * add address type
    
    * remove unused code

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { Config } from './types/config';
 
-export const config: Config = JSON.parse(fs.readFileSync(path.join(__dirname, '../../config.json'), 'utf-8'));
+export const config: Config = JSON.parse(fs.readFileSync(path.join(__dirname, '/../config.json'), 'utf-8'));
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,3 +1,10 @@
 export type Config = {
   node_url: string;
+  database: {
+    host: string;
+    username: string;
+    password: string;
+    db: string;
+    port: string;
+  };
 };
