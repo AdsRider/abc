7c47c69646fe619d456cf2cabd543abf4b254a26
Lurang <lurangjh@naver.com>
2023-04-16 21:07:40 +0900

    minor lint

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import { DatabasePool } from 'slonik';
 import { getBalanceById } from '../../services/balance';
-import { User } from '../../types/user';
 
 const router = express.Router();
 
 export const UserRouter = (pool: DatabasePool) => {
-
   const getBalanceRequestHandler = async (req: express.Request, res: express.Response) => {
     const user = req.user;
     const balance = await getBalanceById(pool, user.id, 'ADS');
