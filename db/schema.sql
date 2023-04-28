0c630df9041d656bfdc387aceb8cfb40e5588ee3
Lurang <lurangjh@naver.com>
2023-04-28 12:42:00 +0900

    withdrawal count를 위한 nonce column추가

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,8 @@ create extension if not exists "uuid-ossp";
 
 create table if not exists address (
     address text primary key,
-    privatekey text not null
+    privatekey text not null,
+    nonce int default 0
 );
 
 create table if not exists "user" (
