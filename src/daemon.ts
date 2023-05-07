2c11ef72a9d56135359c228348758b97e632fa7e
Lurang <lurangjh@naver.com>
2023-05-07 21:48:28 +0900

    making blockchain daemon WIP

--- src/daemon.ts
+++ src/daemon.ts
@@ -1,4 +1,27 @@
+import { connect } from './database/pool';
+import { getBlock, getBlockNumber } from './network/jsonrpc';
+import { getLatestBlockHeight } from './services/block';
+import { sleep } from './util';
 
-export const Daemon = () => {
+export const Daemon = async () => {
+  const pool = await connect();
+
+  while (true) {
+    const topBlockHeight = await getLatestBlockHeight(pool);
+    const blockHeight = await getBlockNumber();
+
+    if (blockHeight - topBlockHeight <= 0) {
+      await sleep(6 * 1000);
+      continue;
+    }
+
+    const diff = Math.min(blockHeight - topBlockHeight, 100);
+    const promise = Array.from({ length: diff, }, (_, index: number) => getBlock(topBlockHeight + index));
+    const blocks = await Promise.all(promise);
+    // const a = blocks.map(b => {
+    //   b
+    //   b.transactions.filter(x => x.to = '')
+    // })
+  }
 
 };
