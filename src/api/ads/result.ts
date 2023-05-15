08f7ccf40abd75e8f2b07bfdb4140ca615d433f7
Lurang <lurangjh@naver.com>
2023-05-15 22:45:19 +0900

    주행결과 기록시 출금처리

--- src/api/ads/result.ts
+++ src/api/ads/result.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import { DatabasePool } from 'slonik';
-import { saveAdsResult } from '../../services/ads';
+import BigNumber from 'bignumber.js';
+import { getAdsById, saveAdsResult } from '../../services/ads';
 import { SaveAdsResultBody, SaveAdsResultDAO } from '../../types/ads';
+import { withdrawalADScoin } from '../../network/jsonrpc';
+import { getSystemAccountByType } from '../../services/address';
+import { saveSpecialLog } from '../../services/dw';
 
 const router = express.Router();
 
@@ -20,7 +24,30 @@ export const AdsResultRouter = (pool: DatabasePool) => {
         end_time: body.end_time,
         user_email: user.email,
       };
-      return await saveAdsResult(conn, dao);
+
+      const ads = await getAdsById(conn, dao.ads_id);
+      // ads.reward는 1000km에 대한 보상
+      // 1km보상 = 전체보상 / 1000
+      // 1인당 최대 10km를 넘을수없음
+      const reward = Math.min(
+        new BigNumber(ads.reward).multipliedBy(dao.meters).dividedBy(1000 * 1000).toNumber(),
+        new BigNumber(ads.reward).dividedBy(100).toNumber(),
+      );
+      await saveAdsResult(conn, dao);
+      const withdrawalAccount = await getSystemAccountByType(pool, 'SYSTEM_WITHDRAWAL');
+      const { address: from, privatekey } = withdrawalAccount;
+      const hash = await withdrawalADScoin(from, user.address, new BigNumber(reward).toString(), privatekey);
+
+      const specialLogDAO = {
+        memo: ['ads_result', ads.id].join('-'),
+        amount: new BigNumber(reward).toString(),
+        user_email: user.email,
+        address: user.address,
+        hash: hash,
+      };
+      await saveSpecialLog(conn, specialLogDAO);
+
+      return hash;
     });
 
     return res.json(result);
