c276d06c8364445c0484aecbc03459c61b76fe2e
Lurang <lurangjh@naver.com>
2023-05-28 14:27:32 +0900

    payment success fail router 분리

--- src/api/payment/index.ts
+++ src/api/payment/index.ts
@@ -1,9 +1,19 @@
 import { DatabasePool } from 'slonik';
 import express from 'express';
 
-const router = express.Router;
+const router = express.Router();
 
 export const PaymentRouter = (pool: DatabasePool) => {
+  const successHandler = async (req: express.Request, res: express.Response) => {
+
+  };
+
+  const failHandler = async (req: express.Request, res: express.Response) => {
+
+  };
+
+  router.post('/success', successHandler);
+  router.post('/fail', failHandler);
 
   return router;
 };
