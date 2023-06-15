3de36769c69946a344831c58e1a51f7242e3d3c0
Lurang <lurangjh@naver.com>
2023-06-16 04:56:22 +0900

    사용자, 광고주, 라이더별 통계 api 작성

--- src/api/statistics/index.ts
+++ src/api/statistics/index.ts
@@ -1,14 +1,43 @@
 import express from 'express';
 import { DatabasePool } from 'slonik';
+import { getAdminStatistics, getAdvertiserStatistics, getNormalUserStatistics } from '../../services/statistics';
 import { ClientError } from '../../util/error';
 import { loginAuthGuard } from '../common';
+
+type StatisticsBody = {
+  from: Date,
+  to: Date,
+};
 
 const router = express.Router();
 
 export const StatisticsRouter = (pool: DatabasePool) => {
 
   const getStatistics = async (req: express.Request, res: express.Response) => {
+    const user = req.session.user!;
+    const level = user.level;
 
+    if (!['광고주', '라이더', '운영자'].includes(level)) {
+      throw new ClientError(400, 'unexpected user');
+    }
+
+    const body = req.body as StatisticsBody;
+    const from = +new Date(body.from);
+    const to = +new Date(body.to);
+
+    if (Number.isNaN(from) || Number.isNaN(to)) {
+      throw new ClientError(400, 'invalid body');
+    }
+
+    const levelFucntionMap = {
+      '광고주': getAdvertiserStatistics,
+      '라이더': getNormalUserStatistics,
+      '관리자': getAdminStatistics,
+    } as const;
+
+    const result = levelFucntionMap[user.level as keyof typeof levelFucntionMap](pool, user.email, from, to);
+
+    return res.json(result);
   };
 
   router.use(loginAuthGuard(pool));
--- src/services/statistics.ts
+++ src/services/statistics.ts
@@ -1,10 +1,128 @@
 import { DatabasePool, DatabaseTransactionConnection, sql } from 'slonik';
 import { z } from 'zod';
+import { transactionObject } from './blockchain';
+import {
+  withdrawalObject,
+  specialLogObject,
+  depositHistoryFragment,
+  specialLogFragment,
+  withdrawalFragment,
+} from './dw';
 
-const getNormalUserStatistics = async (pool: DatabasePool) => {
+// const UserStatisticsResult = z.object({
+//   meters: z.number(),
+//   reward: z.string(),
+//   date: z.date(),
+// });
 
+const UserStatisticsObject = z.object({
+  user_email: z.string(),
+  meters: z.number(),
+  date: z.date(),
+  reward: z.string(),
+});
+
+const getNormalUserStatistics = async (pool: DatabasePool, email: string, from: number, to: number) => {
+  // const data = await pool.any(sql.type(UserStatisticsResult)`
+  //   SELECT sum(meters) as meters, date(end_time)
+  //   FROM (
+  //     SELECT user_email, meters, reward, end_time
+  //     FROM ads_result
+  //     WHERE user_email = ${email}
+  //       AND end_time BETWEEN ${start} AND ${end}
+  //   ) as b
+  //   GROUP BY date(end_time)
+  // `);
+
+  const data = await pool.any(sql.type(UserStatisticsObject)`
+    SELECT user_email, meters, reward, date(end_time)
+    FROM ads_result
+    WHERE user_email = ${email}
+      AND end_time BETWEEN ${from} AND ${to}
+  `);
+
+  // TODO:
+
+  return data;
+};
+
+const AdvertiserStatisticsObject = z.object({ result: z.any() });
+const getAdvertiserStatistics = async (pool: DatabasePool, email: string, from: number, to: number) => {
+  const ads = await pool.one(sql.type(AdvertiserStatisticsObject)`
+    SELECT row_to_json(j) as result FROM (
+      SELECT a.id, a.title, a.subtitle, a.reward, a.image_id, a.start_date, a.end_date, a.user_email,
+        json_agg(json_build_object(
+          'id', r.id,
+          'ads_id', r.ads_id,
+          'path', r.path,
+          'meters', r.meters,
+          'reward', r.reward,
+          'hash', r.hash,
+          'start_time', r.start_time,
+          'end_time', r.end_time
+        )) AS data
+      FROM ads a
+      LEFT JOIN ads_result as r
+        ON a.id = r.ads_id
+      WHERE a.user_email = ${email}
+      GROUP BY a.id
+    ) j
+  `);
+
+  return ads.result;
+};
+
+const getAdminStatistics = async (pool: DatabasePool, email: string, from: number, to: number) => {
+  const specialLog = await pool.any(sql.type(specialLogObject)`
+    SELECT ${specialLogFragment}
+    FROM special_log
+    WHERE timestamp BETWEEN ${from} AND ${to}
+    ORDER BY timestamp
+  `);
+  const specialLogHashList = specialLog.map(x => x.hash);
+  const withdrawal = await pool.any(sql.type(withdrawalObject)`
+    SELECT ${withdrawalFragment}
+    FROM withdrawal
+    WHERE timestamp BETWEEN ${from} AND ${to}
+      AND hash not in (${sql.join(specialLogHashList, sql.fragment`, `)})
+    ORDER BY timestamp
+  `);
+  const deposit = await pool.any(sql.type(transactionObject)`
+    SELECT ${depositHistoryFragment}
+    FROM transaction
+    WHERE timestamp BETWEEN ${from} AND ${to}
+      AND hash not in (${sql.join(specialLogHashList, sql.fragment`, `)})
+    ORDER BY timestamp
+  `);
+
+  const history = [
+    ...withdrawal.map(w => ({
+      // transaction의 amount값에는 +-가 존재하지 않기때문에
+      // withdrawal로그 가져올시 hardcoding함
+      amount: '-' + w.amount,
+      hash: w.hash,
+      timestamp: w.timestamp,
+      type: '출금',
+    })),
+    ...deposit.map(d => ({
+      amount: d.amount,
+      hash: d.hash,
+      timestamp: d.timestamp,
+      type: '입금',
+    })),
+    ...specialLog.map(s => ({
+      amount: s.amount,
+      hash: s.hash,
+      timestamp: s.timestamp,
+      type: s.memo,
+    })),
+  ];
+
+  return history.sort((a, b) => a.timestamp < b.timestamp ? 1 : -1);
 };
 
 export {
   getNormalUserStatistics,
+  getAdvertiserStatistics,
+  getAdminStatistics,
 };
